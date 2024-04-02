3e32b0604a7c0b1e07a2acc732f01052da78866a
ManuAlePacheRu <manualepcheru@gmail.com>
2024-04-02 17:22:19 +0000

    Binary Tree Task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,7 +1,6 @@
 #include "binary_trees.h"
 
 
-
 /**
  * binary_tree_insert_left - asdfg
  *
@@ -13,10 +12,22 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new;
 
+	new = binary_tree_node(parent, value);
 
+	if (!new)
+	{
+		return (NULL);
+	}
 
+	if (parent->left)
+	{
+		new->left = parent->left;
+		(parent->left)->parent = new;
+	}
 
+	parent->left = new;
+
+	return (new);
 }
-
-
