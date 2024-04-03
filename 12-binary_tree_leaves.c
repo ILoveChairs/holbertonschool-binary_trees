3f534d1efd825a63cf61ac11862a91dd512dc5b6
Matias Davezac <8675@holbertonstudents.com>
2024-04-03 17:47:39 +0000

    Added T12. Hey guys, did you know that in terms of male human and female Pokémon breeding, Vaporeon is the most compatible Pokémon for humans? Not only are they in the field egg group, which is mostly comprised of mammals, Vaporeon are an average of 3”03’ tall and 63.9 pounds, this means they’re large enough to be able handle human dicks, and with their impressive Base Stats for HP and access to Acid Armor, you can be rough with one. Due to their mostly water based biology, there’s no doubt in my mind that an aroused Vaporeon would be incredibly wet, so wet that you could easily have sex with one for hours without getting sore. They can also learn the moves Attract, Baby-Doll Eyes, Captivate, Charm, and Tail Whip, along with not having fur to hide nipples, so it’d be incredibly easy for one to get you in the mood. With their abilities Water Absorb and Hydration, they can easily recover from fatigue with enough water. No other Pokémon comes close to this level of compatibility. Also, fun fact, if you pull out enough, you can make your Vaporeon turn white. Vaporeon is literally built for human dick. Ungodly defense stat+high HP pool+Acid Armor means it can take cock all day, all shapes and sizes and still come for more

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,6 +1,32 @@
 #include "binary_trees.h"
 
+/**
+ * _counter - asdfg
+ *
+ * @tree: asdfg
+ *
+ * Return: asdfg
+ */
+size_t _counter(const binary_tree_t *tree)
+{
+	size_t left;
+	size_t right;
 
+	if (tree->left)
+		left = _counter(tree->right);
+	else
+		left = 0;
+
+	if (tree->right)
+		right = _counter(tree->right);
+	else
+		right = 0;
+
+	if ((left == 0) && (right == 0))
+		return (1);
+
+	return (left + right);
+}
 
 /**
  * binary_tree_leaves - asdfg
@@ -11,10 +37,10 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	if (!tree)
+		return (0);
 
-
-
-
+	return (_counter(tree));
 }
 
 
