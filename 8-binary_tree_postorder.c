47d1b9b58d13df58453560040675f2af63e1e3d0
Matias Davezac <8675@holbertonstudents.com>
2024-04-03 16:47:32 +0000

    Added T8. Copied T6 and changed order of print.

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -11,10 +11,16 @@
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
+	if (!tree || !func)
+		return;
 
+	if (tree->left)
+		binary_tree_preorder(tree->left, func);
 
+	if (tree->right)
+		binary_tree_preorder(tree->right, func);
 
-
+	func(tree->n);
 }
 
 
