3e44a2486abf6e98183c3e5bff5310819a76bf0d
Matias Davezac <8675@holbertonstudents.com>
2024-04-03 17:06:32 +0000

    Added T9. Did i do too much?

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,6 +1,34 @@
 #include "binary_trees.h"
+#include <stdlib.h>
 
 
+/**
+ * _heighter - asdfg
+ *
+ * @tree: asdfg
+ *
+ * Return: asdfg
+ */
+size_t _heighter(const binary_tree_t *tree, size_t h)
+{
+	size_t left;
+	size_t right;
+
+	if (tree->left)
+		left = _heighter(tree->left, h + 1);
+	else
+		left = h;
+	
+	if (tree->right)
+		right = _heighter(tree->right, h + 1);
+	else
+		right = h;
+
+	if (left > right)
+		return (left);
+
+	return (right);
+}
 
 /**
  * binary_tree_height- asdfg
@@ -11,10 +39,10 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	if (!tree)
+		return (0);
 
-
-
-
+	return (_heighter(tree, 0));
 }
 
 
