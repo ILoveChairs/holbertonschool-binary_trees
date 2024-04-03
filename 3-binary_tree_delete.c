1671053a761ed103f2892a0a68500281c7809ddb
Matias Davezac <8675@holbertonstudents.com>
2024-04-03 14:14:07 +0000

    Fixed Task 3, now checks if arg is NULL.

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -9,10 +9,15 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
+	if (!tree)
+		return;
+
 	if (tree->left)
 		binary_tree_delete(tree->left);
+
 	if (tree->right)
 		binary_tree_delete(tree->right);
+
 	free(tree);
 }
 
