73e2ba12800b8223cded8a306823c7bbf77946bb
Matias Davezac <8675@holbertonstudents.com>
2024-04-03 17:33:24 +0000

    Added Task 11. Could of done it in one function, but I'm too lazy to transfer everything.

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,6 +1,29 @@
 #include "binary_trees.h"
 
+/**
+ * _sizer - asdfg
+ *
+ * @tree: asdfg
+ *
+ * Return: asdf
+ */
+size_t _sizer(const binary_tree_t *tree)
+{
+	size_t left;
+	size_t right;
 
+	if (tree->right)
+		right = _sizer(tree->right);
+	else
+		right = 0;
+
+	if (tree->left)
+		left = _sizer(tree->left);
+	else
+		left = 0;
+
+	return (right + left + 1);
+}
 
 /**
  * binary_tree_size - asdfg
@@ -11,10 +34,10 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+	if (!tree)
+		return (0);
 
-
-
-
+	return (_sizer(tree));
 }
 
 
