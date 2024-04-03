5ee3157e163c37f5f33a6a31162a3f64b894e34d
Matias Davezac <8675@holbertonstudents.com>
2024-04-03 17:20:08 +0000

    Fixed T7 & T8, copy from T6 left broken recursive due to name disparity

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -17,10 +17,10 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	func(tree->n);
 
 	if (tree->right)
-		binary_tree_preorder(tree->right, func);
+		binary_tree_inorder(tree->right, func);
 
 	if (tree->left)
-		binary_tree_preorder(tree->left, func);
+		binary_tree_inorder(tree->left, func);
 }
 
 
