900a3341b8899ced72b8aa7d82f16408f720fab6
ManuAlePacheRu <manualepacheru@gmail.com>
2024-04-04 14:43:58 +0000

    stashing T14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -16,11 +16,11 @@ int _counter(const binary_tree_t *tree)
 	right = 0;
 
 	if (tree->left)
-		left = _count(tree->left);
+		left = _counter(tree->left);
 	if (tree->right)
-		right = _count(tree->right);
+		right = _counter(tree->right);
 
-	return (left + right);
+	return (left + right + 1);
 }
 
 /**
@@ -30,12 +30,26 @@ int _counter(const binary_tree_t *tree)
  *
  * Return: asdfg
  */
+
 int binary_tree_balance(const binary_tree_t *tree)
 {
 
+	int left;
+	int right;
 
+	if (tree == NULL)
+	{
+		return (0);
+	}
 
+	left = 0;
+	right = 0;
+
+	if (tree->left)
+		left = _counter(tree->left);
+	if (tree->right)
+		right = _counter(tree->right);
+
+	return (left - right);
 
 }
-
-
