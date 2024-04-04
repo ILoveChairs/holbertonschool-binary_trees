4eba1442912795ba22f5df372d26332e23ccd1ab
Matias Davezac <8675@holbertonstudents.com>
2024-04-04 11:21:21 -0300

    Added func to T14. Working in group. Added Counter func which counts n of nodes below.

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,27 @@
 #include "binary_trees.h"
 
+/**
+ * _counter - Counts n of nodes.
+ *
+ * @tree: Root node.
+ *
+ * Return: n of nodes.
+ */
+int _counter(const binary_tree_t *tree)
+{
+	int left;
+	int right;
 
+	left = 0;
+	right = 0;
+
+	if (tree->left)
+		left = _count(tree->left);
+	if (tree->right)
+		right = _count(tree->right);
+
+	return (left + right);
+}
 
 /**
  * binary_tree_balance - asdfg
