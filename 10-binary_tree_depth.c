fca7e634ab1564ccf46b8edcc8c3546a94276a26
ManuAlePacheRu <manualepacheru@gmail.com>
2024-04-04 14:06:21 +0000

    Binary Trees Task 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,7 +1,5 @@
 #include "binary_trees.h"
 
-
-
 /**
  * binary_tree_depth - asdfg
  *
@@ -9,12 +7,17 @@
  *
  * Return: asdfg
  */
+
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 
+	if (!tree->parent)
+	{
+		return (0);
+	}
 
+	else
 
+	return (1 + binary_tree_depth(tree->parent));
 
 }
-
-
