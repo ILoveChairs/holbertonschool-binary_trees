5ec240f8142ac6362b9818330c0592ae53366308
Matias Davezac <8675@holbertonstudents.com>
2024-04-06 22:55:38 +0000

    Added T15. I left it for my peer but i wanted to move on from the project as quickly as possible. We'll talk it out on monday.

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,6 +1,45 @@
 #include "binary_trees.h"
 
+/**
+ * _is_full - asdfg
+ *
+ * @left: asdfg
+ * @right: asdfg
+ *
+ * Return: asdfg
+ */
+int _is_full(int left, int right)
+{
+	if (left && right)
+		return (1);
+	if (left || right)
+		return (0);
+	/* if !both */
+	return (1);
+}
 
+/**
+ * _iterator - asdfg
+ *
+ * @tree: asdfg
+ *
+ * Return: asdfg
+ */
+int _iterator(const binary_tree_t *tree)
+{
+	int left;
+	int right;
+
+	left = 0;
+	right = 0;
+
+	if (tree->left)
+		left = _iterator(tree->left);
+	if (tree->right)
+		right = _iterator(tree->right);
+
+	return (_is_full(left, right));
+}
 
 /**
  * binary_tree_is_full - asdfg
@@ -11,10 +50,10 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+	if (!tree)
+		return (0);
 
-
-
-
+	return (_iterator(tree));
 }
 
 
